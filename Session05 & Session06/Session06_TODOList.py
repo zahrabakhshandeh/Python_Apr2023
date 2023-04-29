75405f2a9ef4bf435e828e4c98a757a8b11c55b4
zahra bakhshandeh <zahra.bakhshandeh1@gmail.com>
2023-04-30 01:18:10 +0330

    added Function Session

--- Session05 & Session06/Session06_TODOList.py
+++ Session05 & Session06/Session06_TODOList.py
@@ -1,3 +1,40 @@
+#...................functions...............
+def add(x):
+	if x not in tasks:
+		tasks.append(x)
+		status.append(False)
+		print("your task has been successfuly added\n")
+	else:
+		print(f"{x}: exist!\n")
+
+def display():
+	for i, t in enumerate(tasks):
+		flag = "undone"
+		if status[i]:
+			flag = "done"
+		print(f"task_{i+1}: {t} ----> {flag}")
+
+def edit(list1):
+	for i in list1:
+		new_task = input("new task: ")
+		if new_task not in tasks:
+			tasks[i] = new_task
+		else:
+			print(f"{new_task}: exist!")
+
+def done(list1):
+	for i in list1:
+		status[i] = True
+
+def search(word):
+	for i, t in enumerate(tasks):
+		if word in t:
+			flag = "undone"
+			if status[i]:
+				flag = "done"
+			print(f"{t} ----> {flag}")
+
+#...............main...............
 tasks = ["hw"] # string
 status = [True] # boolean
 # add, remove, edit, search
@@ -5,48 +42,29 @@ while True:
 	user = input("To-Do List>> Select one option(add/edit/remove/search/display) :")
 	if user == "add":
 		task = input("Enter your task to add: ")
-		if task not in tasks:
-			tasks.append(task)
-			status.append(False)
-			print("your task has been successfuly added\n")
-		else:
-			print(f"{task}: exist!\n")
+		add(task)
 	elif user == "display":
-		for i, t in enumerate(tasks):
-			# task_1 = python ----> done
-			# True ===> done, False ===> undone
-			flag = "undone"
-			if status[i]:
-				flag = "done"
-			print(f"task_{i+1}: {t} ----> {flag}")
+		display()
 	elif user == "edit":
+		display()
 		index = input("index of task: ") # 1 2 3 4
 		list1 = [int(i)-1 for i in index.split()]
-		for i in list1:
-			new_task = input("new task: ")
-			if new_task not in tasks:
-				tasks[i] = new_task
-			else:
-				print(f"{new_task}: exist!")
+		edit(list1)
 	elif user == "done":
+		display()
 		index = input("index of task: ") # 1 2 3 4
 		list1 = [int(i)-1 for i in index.split()]
-		for i in list1:
-			status[i] = True
+		done(list1)
 	elif user == "remove":
-		index = input("index of task: ") # 1 2 3 4
+		"""index = input("index of task: ") # 1 2 3 4
 		list1 = [int(i)-1 for i in index.split()]
 		for i in list1:
 			tasks.pop(i)
-			status.pop(i)
+			status.pop(i)"""
+		pass
 	elif user == "search":
 		word = input("task to search: ")
-		for i, t in enumerate(tasks):
-			if word in t:
-				flag = "undone"
-				if status[i]:
-					flag = "done"
-				print(f"{t} ----> {flag}")
+		search(word)
 	elif user == "":
 		pass
 	elif user == "exit":
